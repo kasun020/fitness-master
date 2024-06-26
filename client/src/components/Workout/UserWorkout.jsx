44bab68231c74807022cfca06f31423bb2811f89
kasun020 <157946129+kasun020@users.noreply.github.com>
2024-06-26 23:46:41 +0530

    frontend code for display workout

---
+++ client/src/components/Workout/UserWorkout.jsx
@@ -0,0 +1,50 @@
+// Workout.js
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./UserWorkout.css";
+
+const Workout = () => {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/workouts")
+      .then((response) => {
+        setWorkouts(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the workouts!", error);
+      });
+  }, []);
+
+  return (
+    <div className="workout-container">
+      {workouts.map((workout, index) => (
+        <div className="workout-card" key={index}>
+          <h2>{workout.scheduleType}</h2>
+          <p>
+            <strong>Username:</strong> {workout.Username}
+          </p>
+          <p>
+            <strong>Instructions:</strong> {workout.Instructions}
+          </p>
+          <p>
+            <strong>Day 1 Workout:</strong> {workout.day1workout}
+          </p>
+          {workout.day2workout && (
+            <p>
+              <strong>Day 2 Workout:</strong> {workout.day2workout}
+            </p>
+          )}
+          {workout.day3workout && (
+            <p>
+              <strong>Day 3 Workout:</strong> {workout.day3workout}
+            </p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Workout;
